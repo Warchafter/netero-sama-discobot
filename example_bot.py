50f60391c257c44ccb471990cf9f4e441ee5aff7
Warchafter <kevin.arriaga@gmail.com>
2023-11-29 14:16:03 -0500

    Added more listeners...

--- example_bot.py
+++ example_bot.py
@@ -13,41 +13,48 @@ intents.typing = False
 intents.presences = False
 intents.message_content = True
 intents.guild_scheduled_events = True
+intents.members = True
 
-client = discord.Client(intents=intents)
+bot = discord.Client(intents=intents)
 
 # client.run(token, log_handler=handler)
 discord.utils.setup_logging()
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content.startswith('El bot'):
         await message.channel.send('Si soy!')
 
-@client.event
+@bot.event
 async def print_message(event):
     print(event.content)
 
-# @client.event
+# @bot.event
 # async def on_message(message):
-#     if message.author == client.user:
+#     if message.author == bot.user:
 #         return
 
 #     if message.content.startswith('$Events'):
 #         await fetch_scheduled_events(*, with_counts=True)
 
-@client.event
-async def on_scheduled_event_created(event):
-    await discord.on_scheduled_event_create(event)
-    await event.channel.send(f'An event was just created by ${event.creator}!')
+@bot.event
+async def on_member_join(self, member):
+    guild = member.guild
+    if guild.system_channel is not None:
+        to_send = f'Welcome {member.mention} to {guild.name}!'
+        await guild.system_channel.send(to_send)
+
+@bot.event
+async def on_scheduled_event_create(self, event):
+    await event.system_channel.send(f'An event was just created by ${event.creator}!')
 
 
-client.run(os.getenv('DISCORD_BOT_TOKEN'))
+bot.run(os.getenv('DISCORD_BOT_TOKEN'))
